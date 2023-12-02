f9d2a12001b5d4dfc65ceb40cf2072bb952c8810
Tobias Schmücker <10495193+schmuecker@users.noreply.github.com>
2023-12-02 15:47:52 +0000

    Update 4 files

---
+++ src/02/02-01.ts
@@ -0,0 +1,44 @@
+async function readFile(path: string): Promise<string> {
+  const file = await Bun.file(path);
+  return file.text();
+}
+
+function getLines(input: string): string[] {
+  return input.split("\n");
+}
+
+type Round = {
+  red: number;
+  blue: number;
+  green: number;
+  valid: boolean;
+};
+
+type Game = {
+  id: number;
+  rounds: Round[];
+};
+
+function parseGame(line: string): Game {}
+
+function isRoundValid(round: Round): boolean {}
+
+function isGameValid(game: Game): boolean {}
+
+function sum(numbers: number[]): number {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+async function main() {
+  console.log("Day 02 - Challenge 1");
+  const fileContent = await readFile("./src/02/input.txt");
+  const lines = getLines(fileContent);
+  const validGames = [];
+  // Mappe über lines
+  //   Pro Line, schau ob alle Rounds des Games valid sind
+  //   Sammle IDs von valid games
+  // Sum der valid game ids
+  console.log(lines);
+}
+
+main();
